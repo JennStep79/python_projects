9b5e7859d68d816ef8d5030e531213d5ece08e87
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-03-20 19:41:43 -0700

    practice with classes

--- activity_3-1.py
+++ activity_3-1.py
@@ -1,42 +1,57 @@
-# Example base class
 class Car:
-    # Use the __init__ method to assign values to object properties
-    def __init__(self, name):
-        self.name = name
-    # Custom method with no extra parameters
-    def start(self):
-        print("Vroom!")
-    # Custom method with one parameter
-    def talk(self, driver):
-        print(f"Greetings {driver}, I am {self.name}!")
-    # Class attribute
-    gravity = -9.8
+  def __init__(self, name, max_speed):
+    self.name = name
+    self.max_speed = max_speed
 
-# Example inherited class
-class RobotCar(Car):
-    # Use the __init__ method to assign values to object properties
-    def __init__(self, name, speed, type):
-        # Call the __init__ method of the parent class since name is already a property
-        super().__init__(name)
-        # New properties of the inherited class
-        self.speed = speed
-        self.type = type
+  def start(self):
+    print('Vroom!')
 
-# Create base class instance
-my_car = Car("Kitt")
-# Create inherited class instance
-my_car_2 = RobotCar("Speedy", 180, "sentient")
+  def talk(self, driver):
+    print(f'Hello, {driver}, I am {self.name}.')
 
-# Call the custom method
-my_car.start()
-my_car.talk("John")
+# my_car = Car('Kitt', 180)
+# my_other_car = Car('Speedy', 55)
 
-# Print the class attributes
-print(my_car.name)
-print(my_car.gravity)
+# my_car.talk('Michael')
 
-# Call inherited method
-my_car_2.start()
+class Race:
+  def __init__(self, name, driver_limit):
+    self.name = name
+    self.driver_limit = driver_limit
+    self.drivers = []
+  def add_driver(self, driver):
+    if len(self.drivers) < self.driver_limit:
+      self.drivers.append(driver)
+  def get_average_ranking(self):
+    rank_sum = 0
+    for driver in self.drivers:
+      rank_sum += driver.get_ranking()
+    return rank_sum / len(self.drivers)
 
-# Print inherited attributes
-print(my_car_2.speed)
+seattle_500 = Race('Seattle 500', 4)
+# print(my_course.name, my_course.driver_limit, my_course.drivers)
+
+class Driver:
+  number_of_drivers = 0
+  def __init__(self, name, age, ranking):
+    self.name = name
+    self.age = age
+    self.ranking = ranking
+    self.number_of_drivers += 1
+  def get_ranking(self):
+    return self.ranking
+  
+dale = Driver('Dale Earnhardt', 29, 100)
+lewis = Driver('Lewis Hamilton', 36, 83)
+eilud = Driver('Eilud Kichoge', 36, 95)
+sebastian = Driver('Sebastian Vettel', 34, 76)
+ayrton = Driver('Ayrton Senna', 34, 99)
+
+seattle_500.add_driver(dale)
+seattle_500.add_driver(lewis)
+seattle_500.add_driver(eilud)
+seattle_500.add_driver(sebastian)
+seattle_500.add_driver(ayrton)
+print(seattle_500.get_average_ranking())
+
+
