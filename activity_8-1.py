60b49bcef5d79bc8ed65f60ef0f708ec2b4e84b8
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-04-01 11:38:34 -0700

    practice with bubble sort algorithm

---
+++ activity_8-1.py
@@ -0,0 +1,17 @@
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        swapped = False
+        print(f'Iteration {i}')
+        for j in range(len(lst)-1):
+            print(f'Comparing {lst[j]} and {lst[j+1]}')
+            if lst[j] > lst[j+1]:
+                swapped = True
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+        if not swapped:
+            return
+    return lst
+
+sample_list = [1, 5, 2, 6, 7]
+print(f"Unsorted list: {sample_list}")
+bubble_sort(sample_list)
+print(f"Sorted list: {sample_list}")
